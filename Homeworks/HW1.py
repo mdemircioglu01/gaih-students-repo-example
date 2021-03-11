3193595ad23891b4a3a681ed5c4e77a85ecfc2cd
mdemircioglu01 <80275424+mdemircioglu01@users.noreply.github.com>
2021-03-11 16:35:25 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,11 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+cift = [x for x in range(30) if x % 2 == 0]
+tek = [x for x in range(30) if x % 2 == 1]
+liste =cift+tek
+cliste = [x*2 for x in liste]
+i=0
+while (i < len(cliste)):
+  print(i, ". eleman: ///", "değeri:", cliste[i], "/// sınıfı:", type(cliste[i]))
+  i+=1
