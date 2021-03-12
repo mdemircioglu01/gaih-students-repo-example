2ab3488ae0bad4f122ea7dcdfad1fb58a1bf3c86
mdemircioglu01 <80275424+mdemircioglu01@users.noreply.github.com>
2021-03-12 15:09:48 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,13 @@
-#Explain your work
+#CV uygulaması: 5 sözlük oluşturularak 5 farklı kişinin bilgileri oluşturulmuş ve ekrana print edilmiştir.
 
-#Question 1
-for x in range(a):
-	print(a)
+#Ödev 2
+cv1 ={"isim":"Sergen", "soyad":"Yalçın", "yas":"30", "üniversite":"Çukurova", "programlama":"Python"}
+cv2 ={ "isim":"Murat", "soyad":"Demir", "yas":"25", "üniversite":"İstanbul", "programlama":"C++"}
+cv3 ={"isim":"Ayşe", "soyad":"Uzun", "yas":"33", "üniversite":"9 Eylül", "programlama":"Python"}
+cv4 ={"isim":"İsmail", "soyad":"Bulgur", "yas":"27", "üniversite":"Çukurova", "programlama":"C++"}
+cv5 ={"isim":"Kenan", "soyad":"Kalav", "yas":"23", "üniversite":"Boğazici", "programlama":"Visual Basic"}
+print( " Ad Soyad: "+cv1["isim"]+" "+cv1["soyad"]+"   Yaş:"+cv1["yas"]+"  Üniversite:"+cv1["üniversite"]+" Üniversitesi"+"  Bildiği Programlama Dili: "+cv1["programlama"])
+print(" Ad Soyad: "+cv2["isim"]+" "+cv2["soyad"]+"   Yaş:"+cv2["yas"]+"  Üniversite:"+cv2["üniversite"]+" Üniversitesi"+"  Bildiği Programlama Dili: "+cv2["programlama"])
+print( " Ad Soyad: "+cv3["isim"]+" "+cv3["soyad"]+"   Yaş:"+cv3["yas"]+"  Üniversite:"+cv3["üniversite"]+" Üniversitesi"+"  Bildiği Programlama Dili: "+cv3["programlama"])
+print( " Ad Soyad: "+cv4["isim"]+" "+cv4["soyad"]+"   Yaş:"+cv4["yas"]+"  Üniversite:"+cv4["üniversite"]+" Üniversitesi"+"  Bildiği Programlama Dili: "+cv4["programlama"])
+print( " Ad Soyad: "+cv5["isim"]+" "+cv5["soyad"]+"   Yaş:"+cv5["yas"]+"  Üniversite:"+cv5["üniversite"]+" Üniversitesi"+"  Bildiği Programlama Dili: "+cv5["programlama"])
